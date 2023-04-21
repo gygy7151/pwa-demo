6151c8c855ee2938ed9f0a45198a2bd7e146f259
gygy7151 <gygy7151@gmail.com>
2023-04-21 16:21:38 +0900

    fix

--- push.js
+++ push.js
@@ -1,11 +1,15 @@
 var push = require('web-push')
 
-let vapIdKeys = push.generateVAPIDKeys();
+const keys = push.generateVAPIDKeys();
 
-console.log(vapIdKeys);
+let vapIdKeys = {
+    publicKey: 'BD6Y5yIe1LF9VdWqkbvwZny7Y-eScTmHEokauEKektdv6FAzEkUmx8iVOlERTcF_yIWX5y7FZwEBeg-U3o74NPc',
+    privateKey: 'sc5t5HCgHtFXhWPaTHjsntWntNo3vlLeS6tp5TvwzMA'
+  }
 
-push.setVapidDetails('https://www.google.com', vapIdKeys.publicKey, vapIdKeys.privateKey)
+  
+push.setVapidDetails('mailto:test@code.co.uk', vapIdKeys.publicKey, vapIdKeys.privateKey)
 
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/clSGdqMU_-w:APA91bFOxbE1UdZ5kQxP0vtYsxk6It-r3OgMPk7GzUQkLYy151ohlQmtEGV9srBsnSR7sDnYoZGWudVa8WiYmucYkw9yIp2Wc79RXbSikQ7zYxDHuRzJXHSLitqGsazQa_TMm4-meL9G","expirationTime":null,"keys":{"p256dh":"BGXMOps4z0gY-iOaZsEoSka27V-r_TEKmgg1dXz3n1G7HADedz627JBx9LxA3-pYG4zwVJ1DF2YkSTMFkoGRxTA","auth":"nxbzOwtUPSDJDuEBtJra9Q"}}
+let sub = {};
 
 push.sendNotification(sub, 'test message')
