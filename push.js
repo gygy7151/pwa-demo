1fa63897b4dcbadcab1f42ed55c2182b5e121f40
gygy7151 <gygy7151@gmail.com>
2023-04-21 14:41:19 +0900

    fix

--- push.js
+++ push.js
@@ -7,6 +7,13 @@ let vapIdKeys = {
 
 push.setVapidDetails('welcome IOS push notification!', vapIdKeys.publicKey, vapIdKeys.privateKey)
 
-let sub = {};
+let sub = {
+    endpoint:"https://fcm.googleapis.com/fcm/send/clSGdqMU_-w:APA91bFOxbE1UdZ5kQxP0vtYsxk6It-r3OgMPk7GzUQkLYy151ohlQmtEGV9srBsnSR7sDnYoZGWudVa8WiYmucYkw9yIp2Wc79RXbSikQ7zYxDHuRzJXHSLitqGsazQa_TMm4-meL9G",
+    expirationTime:null,
+    keys:{
+        p256dh:"BGXMOps4z0gY-iOaZsEoSka27V-r_TEKmgg1dXz3n1G7HADedz627JBx9LxA3-pYG4zwVJ1DF2YkSTMFkoGRxTA",
+        auth:"nxbzOwtUPSDJDuEBtJra9Q"
+    }
+}
 
 push.sendNotification(sub, 'test message')
